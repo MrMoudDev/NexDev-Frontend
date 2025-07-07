7c29d496664f148d64a79803930bb5334cccf315
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-07-07 11:18:11 -0500

    [+] register.component: Formulario Reactivo Anidado

--- src/app/pages/public/register/register.ts
+++ src/app/pages/public/register/register.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+  public formData!: FormGroup;
+  public roles: string[] = [ 'company', 'developer' ];
 
+  constructor() {
+    this.formData = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl(),
+      rol: new FormControl(),
+      developer: new FormGroup({
+        nombre: new FormControl(),
+        stack: new FormControl(),
+        portafolioUrl: new FormControl(),
+        cvUrl: new FormControl()
+      }),
+      company: new FormControl({
+        nombre: new FormControl(),
+        descripcion: new FormControl(),
+        logoUrl: new FormControl(),
+        sitioWeb: new FormControl()
+      })
+    })
+  }
+
+  onSubmit() {
+    if( this.formData.valid ) {
+      console.log( this.formData.value );
+    }
+  }
 }
