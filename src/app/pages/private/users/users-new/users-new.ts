49af9b9d5aa977bcb0016dbe6889d8032d83a534
MrMoudDev <santiago.velasquez08120@gmail.com>
2025-07-11 21:23:08 -0500

    Register user

--- src/app/pages/private/users/users-new/users-new.ts
+++ src/app/pages/private/users/users-new/users-new.ts
@@ -1,11 +1,73 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { User } from '../../../../services/user';
 
 @Component({
   selector: 'app-users-new',
-  imports: [],
+  imports: [ ReactiveFormsModule],
   templateUrl: './users-new.html',
   styleUrl: './users-new.css'
 })
 export class UsersNew {
+  public formData!: FormGroup;
+  public roles: string[] = [ 'company', 'developer' ];
 
+  constructor( private userService:User) {
+    this.formData = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl(),
+      rol: new FormControl(),
+      developer: new FormGroup({
+        nombre: new FormControl(),
+        stack: new FormControl(),
+        experiencia: new FormControl(),
+        portafolioUrl: new FormControl(),
+        cvUrl: new FormControl()
+      }),
+      company: new FormGroup({
+        nombre: new FormControl(),
+        descripcion: new FormControl(),
+        logoUrl: new FormControl(),
+        sitioWeb: new FormControl()
+      })
+    })
+  }
+
+  ngOnInit() {
+    this.onToggleRol();   // Activa o desactiva formularios anidados por defecto
+    this.formData.get( 'rol' )?.valueChanges.subscribe( () => this.onToggleRol() );
+  }
+
+  onSubmit() {
+    if( this.formData.valid ) {
+      console.log( this.formData.value);
+      this.userService.registerUser(this.formData.value).subscribe({
+        next: (data) => {
+          console.log(data);
+        },
+        error: (error) => {
+          console.error(error)
+        },
+        complete: () => {
+          this.formData.reset()
+        }
+      })
+    }
+  }
+
+  onToggleRol() {
+    const rol = this.formData.get( 'rol' )?.value;
+
+    const formDataDeveloper = this.formData.get( 'developer' ) as FormGroup;
+    const formDataCompany = this.formData.get( 'company' ) as FormGroup;
+
+    if( rol == 'developer' ) {
+      formDataDeveloper.enable();
+      formDataCompany.disable();
+    }
+    else {
+      formDataDeveloper.disable();
+      formDataCompany.enable();
+    }
+  }
 }
---
+++ src/app/services/company.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Company {
+
+  constructor( private http:HttpClient ) { }
+
+  registerUser ( newUser: any ) {
+    return this.http.post( 'http://localhost:3000/api/users/', newUser )
+  }
+  getUser ( ) {
+    return this.http.get( 'http://localhost:3000/api/users/')
+  }
+  getUserById ( id:string ) {
+    return this.http.get( 'http://localhost:3000/api/users/' + id)
+  }
+  deleteByIdUser ( id: string ) {
+    return this.http.delete( 'http://localhost:3000/api/users/' + id)
+  }
+  updateByIdUser ( Id: string, updateUser:any ) {
+    return this.http.delete( 'http://localhost:3000/api/users/' + Id, updateUser)
+  }
+}
