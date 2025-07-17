66b8d81355cc47779a01f2ed9e88947869a3cb3f
Joseph Alejandro Cardenas Palacio <joseph.cardenas2005@gmail.com>
2025-07-17 14:33:08 -0500

    no tengo ni idea y algo de diseño de las tarjetas post: Santiago alias esperancita resuelve si hay conflicto

--- src/app/pages/public/vacants/vacants.ts
+++ src/app/pages/public/vacants/vacants.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 import { Vacant } from '../../../services/vacant';
 import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vacants',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './vacants.html',
   styleUrl: './vacants.css'
 })
 export class Vacants {
+public formData!:FormGroup;
+  vacant: any[] = [];
 
-
-      vacant: any[] = [];
-
-    constructor ( private aplicationService: Vacant) {}
+    constructor ( private aplicationService:Vacant) {
+      this.formData=new FormGroup({
+        tituloV: new FormControl(),
+        descriptionV: new FormControl(),
+        tecnologiasV: new FormControl(),
+      })
+    }
       ngOnInit () {
-        console.log('aaaaaaa')
         this.aplicationService.getVacant().subscribe({
           next: (data) => {
             this.vacant = data
