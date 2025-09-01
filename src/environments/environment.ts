ba5b8542c4b1427b7c2b19995a11bb3aa8dd8e77
alejandro <alejandrocristhian823@gmail.com>
2025-09-01 18:59:33 -0500

    m

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,4 @@
 export const environment = {
     production: true,
-
-    apiURL: "http://ec2-3-144-173-83.us-east-2.compute.amazonaws.com:3000/api",
-
-    apiUrl: 'http://ec2-54-224-243-203.compute-1.amazonaws.com:3000/api'
-
+    apiUrl: 'http://ec2-3-144-173-83.us-east-2.compute.amazonaws.com:3000/api'
 };
